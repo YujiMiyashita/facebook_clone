78095b4a6d4ea8b02929356e7c66de02af51e1ac
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-08-27 12:11:32 +0900

    SNS設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,5 +17,9 @@ Devise.setup do |config|
     config.omniauth :twitter, ENV["TWITTER_ID_DEVELOPMENT"], ENV["TWITTER_SECRET_DEVELOPMENT"]
     config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"],
     scope: 'email', display: 'popup', info_fields: 'name, email'
+  elsif Rails.env.production?
+    config.omniauth :twitter, ENV["TWITTER_ID_PRODUCTION"], ENV["TWITTER_SECRET_PRODUCTION"]
+    config.omniauth :facebook, ENV["FACEBOOK_ID_PRODUCTION"], ENV["FACEBOOK_SECRET_PRODUCTION"],
+    scope: 'email', display: 'popup', info_fields: 'name, email'
   end
 end
