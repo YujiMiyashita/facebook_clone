85c63f1fd318518609ac490010bf4ec701643dec
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-09-06 13:36:43 +0900

    ホゲホゲ

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@
   Topic.create(
     title: "Title#{n}",
     content: "Content#{n}",
-    user_id: (n + 1)
+    user_id: (n + 10)
   )
 
   Comment.create(
     content: "Content#{n}",
-    user_id: (n + 1),
-    topic_id: (n + 1)
+    user_id: (n + 10),
+    topic_id: (n + 3)
   )
 end
